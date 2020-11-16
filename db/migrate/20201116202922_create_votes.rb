5e37022c687a78a5f21e2ab6aefff40707a6aea4
Nick Edwin <nickedwin85@gmail.com>
2020-11-16 15:18:22 -0700

    Vote table for db

---
+++ db/migrate/20201116202922_create_votes.rb
@@ -0,0 +1,9 @@
+class CreateVotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :votes do |t|
+      t.string :movie_title
+      t.integer :thumbs_up, default: 0
+      t.integer :thumbs_down, default: 0
+    end
+  end
+end
