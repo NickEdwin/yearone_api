9fee5e8505c724a8fa455c57f45292423de9b788
Nick Edwin <nickedwin85@gmail.com>
2020-11-16 13:15:23 -0700

    Methods for movie show page information

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,18 @@
 class MoviesController < ApplicationController
   def top_rated
-    @movies = Movie.top_rated
+    @movies = MovieFacade.top_rated
   end
 
   def search
-    @movies = Movie.search(params[:find_movies])
+    @movies = MovieFacade.search(params[:find_movies])
+  end
+
+  def show
+    @movie = MovieFacade.details(params[:id])
+    @cast = MovieFacade.cast(params[:id])
+    @crew = MovieFacade.crew(params[:id])
+    @reviews = MovieFacade.reviews(params[:id])
+    @trailer = MovieFacade.videos(params[:id])
+    @recommended = MovieFacade.recommended(params[:id])
   end
 end
