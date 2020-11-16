c20ed5cc64cf3f101a3e66996476657deec7c692
Nick Edwin <nickedwin85@gmail.com>
2020-11-16 10:16:15 -0700

    Movie search route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index', as: :root
 
   get '/movies/top_rated', to: 'movies#top_rated'
+
+  post '/movies/search', to: 'movies#search'
+
 end
