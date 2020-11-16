6a09664738cb539a8b33e5c87ed8130dac570f22
Nick Edwin <nickedwin85@gmail.com>
2020-11-16 10:16:00 -0700

    Movie search spec

--- spec/features/landing_page/landing_page_spec.rb
+++ spec/features/landing_page/landing_page_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'As any user' do
   describe "On the discover page" do
     it "should have a button to discover top movies and a search bar", :vcr do
       visit "/"
+
       expect(page).to have_button("Find Top Rated Movies")
       expect(page).to have_field('Search by movie title')
       expect(page).to have_button("Find Movies")
@@ -16,6 +17,16 @@ RSpec.describe 'As any user' do
       expect(current_path).to eq('/movies/top_rated')
       expect(page).to have_content("Title: Gabriel's Inferno Part II")
       expect(page).to have_content("Average Rating: 8.9")
+    end
+
+    it "can search for a movie based on keywords", :vcr do
+      visit "/"
+
+      fill_in :find_movies, with: "Spider Man"
+      click_on "Find Movies"
+
+      expect(current_path).to eq("/movies/search")
+      expect(page).to have_content("Title: Spider-Man")
     end
   end
 end
